55aaaedbec6c6bac97b848ca496f0d77f6b0b4b4
Hamal Saefudin <hamalsaefudin48@gmail.com>
2021-11-19 16:32:46 +0700

    connect mongo

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,7 @@ const envVarsSchema = Joi.object()
     .keys({
         NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
         PORT: Joi.number().default(3000),
+        MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     })
     .unknown();
 
@@ -20,4 +21,11 @@ if (error) {
 module.exports = {
     env: envVars.NODE_ENV,
     port: envVars.PORT,
+    mongoose: {
+        url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+    },
 };
