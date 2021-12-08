625813bbc2135dfabf2cc5e4c274a44baf1c6621
Hamal Saefudin <hamalsaefudin48@gmail.com>
2021-12-08 10:56:00 +0700

    add controller to router author

--- src/controllers/author.controller.js
+++ src/controllers/author.controller.js
@@ -2,20 +2,42 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const authorService = require('../services/author.service');
 const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
 
 const createAuthor = catchAsync(async (req, res) => {
     const author = await authorService.createAuthor(req.body);
     res.status(httpStatus.CREATED).json(author);
 });
 
-const getAuthor = catchAsync(async (req, res) => {
-    const filter = pick(req.query, ['name', 'role']);
+const getAuthors = catchAsync(async (req, res) => {
+    const filter = pick(req.query, ['name', 'speciality']);
     const options = pick(req.query, ['sortBy', 'limit', 'page']);
     const authors = await authorService.getAuthors(filter, options);
     res.json(authors);
 });
 
+const getAuthor = catchAsync(async (req, res) => {
+    const author = await authorService.getAuthorById(req.params.authorId);
+    if (!author) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Author Not Found');
+    }
+    res.send(author);
+});
+
+const updateAuthor = catchAsync(async (req, res) => {
+    const author = await authorService.updateAuthorById(req.params.id, req.body);
+    res.send(author);
+});
+
+const deleteAuthor = catchAsync(async (req, res) => {
+    await authorService.deleteAuthor(req.params.id);
+    res.status(httpStatus.NO_CONTENT).send();
+});
+
 module.exports = {
     createAuthor,
+    getAuthors,
     getAuthor,
+    updateAuthor,
+    deleteAuthor,
 };
